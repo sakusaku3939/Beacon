ef8156931e5019684a774f99a13e35e118ef4b6f
AokiYuki <yuukiyuuki223@gmail.com>
2020-11-17 01:43:22 +0900

    設定ナビゲーションの追加

--- app/src/main/java/com/sakusaku/beacon/BeaconActivity.java
+++ app/src/main/java/com/sakusaku/beacon/BeaconActivity.java
@@ -20,7 +20,7 @@ public class BeaconActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_search, R.id.navigation_facility)
+                R.id.navigation_home, R.id.navigation_search, R.id.navigation_facility, R.id.navigation_setting)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
