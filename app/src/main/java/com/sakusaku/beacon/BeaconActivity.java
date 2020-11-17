901f8860697decbd3317285b006b2d86ae572144
AokiYuki <yuukiyuuki223@gmail.com>
2020-11-18 02:41:34 +0900

    ツールバーの追加

--- app/src/main/java/com/sakusaku/beacon/BeaconActivity.java
+++ app/src/main/java/com/sakusaku/beacon/BeaconActivity.java
@@ -17,13 +17,7 @@ public class BeaconActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beacon);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_location, R.id.navigation_search, R.id.navigation_account)
-                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
