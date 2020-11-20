a2490edf51188d701df0d52ae85baea97cabefbd
AokiYuki <yuukiyuuki223@gmail.com>
2020-11-21 00:03:49 +0900

    FABの追加

--- app/src/main/java/com/sakusaku/beacon/BeaconActivity.java
+++ app/src/main/java/com/sakusaku/beacon/BeaconActivity.java
@@ -2,13 +2,14 @@ package com.sakusaku.beacon;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BeaconActivity extends AppCompatActivity {
 
@@ -17,8 +18,17 @@ public class BeaconActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beacon);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavController navController = getNavController();
         NavigationUI.setupWithNavController(navView, navController);
     }
 
+    @NonNull
+    private NavController getNavController() {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        if (!(fragment instanceof NavHostFragment)) {
+            throw new IllegalStateException("Activity " + this
+                    + " does not have a NavHostFragment");
+        }
+        return ((NavHostFragment) fragment).getNavController();
+    }
 }
