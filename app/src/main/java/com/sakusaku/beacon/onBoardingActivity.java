5a775858300caf6ee910a6320f8676f2a408df22
AokiYuki <yuukiyuuki223@gmail.com>
2020-11-28 21:02:21 +0900

    オンボーディング画面2の追加

--- app/src/main/java/com/sakusaku/beacon/onBoardingActivity.java
+++ app/src/main/java/com/sakusaku/beacon/onBoardingActivity.java
@@ -2,14 +2,19 @@ package com.sakusaku.beacon;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.jgabrielfreitas.core.BlurImageView;
 import com.sakusaku.beacon.onBoarding.FirstFragment;
+import com.sakusaku.beacon.onBoarding.SecondFragment;
 
 public class onBoardingActivity extends AppCompatActivity {
+
+    private int fragment_state = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,7 @@ public class onBoardingActivity extends AppCompatActivity {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.add(R.id.onboarding_fragment, firstFragment);
         transaction.commit();
+        fragment_state = 1;
     }
 
     @Override
@@ -31,9 +37,39 @@ public class onBoardingActivity extends AppCompatActivity {
         super.onPause();
     }
 
+    @Override
+    public void onBackPressed() {
+        onClickBack(null);
+    }
+
+    private void fragmentBack() {
+
+    }
+
     public void onClickGetStarted(View view) {
-        Intent intent = new Intent(getApplication(), onBoardingActivity.class);
-        startActivity(intent);
-        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+        BlurImageView blurImage = findViewById(R.id.appCompatImageView);
+        new Handler().postDelayed(() -> {
+            blurImage.setBlur(5);
+        }, 100);
+        SecondFragment secondFragment = new SecondFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.setCustomAnimations(R.anim.fragment_close_enter, R.anim.fragment_close_exit);
+        transaction.addToBackStack(null);
+        transaction.replace(R.id.onboarding_fragment, secondFragment);
+        transaction.commit();
+        fragment_state = 2;
+    }
+
+
+    public void onClickBack(View view) {
+        getSupportFragmentManager().popBackStack();
+        fragment_state--;
+        if (fragment_state == 1) {
+            BlurImageView blurImage = findViewById(R.id.appCompatImageView);
+            blurImage.setBlur(0);
+        }
+    }
+
+    public void onClickNext(View view) {
     }
 }
