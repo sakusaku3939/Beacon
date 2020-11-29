909aafcf8e5453d2a48b9192725e8c61fc829ac9
AokiYuki <yuukiyuuki223@gmail.com>
2020-11-29 23:53:21 +0900

    オンボーディング画面3の追加

--- app/src/main/java/com/sakusaku/beacon/onBoardingActivity.java
+++ app/src/main/java/com/sakusaku/beacon/onBoardingActivity.java
@@ -1,20 +1,24 @@
 package com.sakusaku.beacon;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.jgabrielfreitas.core.BlurImageView;
 import com.sakusaku.beacon.onBoarding.FirstFragment;
 import com.sakusaku.beacon.onBoarding.SecondFragment;
+import com.sakusaku.beacon.onBoarding.ThirdFragment;
 
 public class onBoardingActivity extends AppCompatActivity {
 
-    private int fragment_state = 0;
+    private int fragment_state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,28 +46,17 @@ public class onBoardingActivity extends AppCompatActivity {
         onClickBack(null);
     }
 
-    private void fragmentBack() {
-
-    }
-
     public void onClickGetStarted(View view) {
         BlurImageView blurImage = findViewById(R.id.appCompatImageView);
         new Handler().postDelayed(() -> {
             blurImage.setBlur(5);
         }, 100);
-        SecondFragment secondFragment = new SecondFragment();
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.setCustomAnimations(R.anim.fragment_close_enter, R.anim.fragment_close_exit);
-        transaction.addToBackStack(null);
-        transaction.replace(R.id.onboarding_fragment, secondFragment);
-        transaction.commit();
-        fragment_state = 2;
+        FragmentTransaction(new SecondFragment(), 2);
     }
-
 
     public void onClickBack(View view) {
         getSupportFragmentManager().popBackStack();
-        fragment_state--;
+        fragment_state -= fragment_state > 0 ? 1 : 0;
         if (fragment_state == 1) {
             BlurImageView blurImage = findViewById(R.id.appCompatImageView);
             blurImage.setBlur(0);
@@ -71,5 +64,32 @@ public class onBoardingActivity extends AppCompatActivity {
     }
 
     public void onClickNext(View view) {
+        switch (fragment_state) {
+            case 2:
+                EditText text = findViewById(R.id.appCompatEditText);
+                if (text.getText().toString().isEmpty()) {
+                    text.setError("文字を入力してください");
+                } else {
+                    RadioGroup radioGroup = findViewById(R.id.onboardingRadio);
+                    int id = radioGroup.getCheckedRadioButtonId();
+                    RadioButton radioButton = findViewById(id);
+                    if (radioButton.getText().toString().equals("先生")) {
+                        FragmentTransaction(new ThirdFragment(), 3);
+                    } else {
+                    }
+                }
+                break;
+            case 3:
+                break;
+        }
+    }
+
+    private void FragmentTransaction(Fragment fragment, int state) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.setCustomAnimations(R.anim.fragment_close_enter, R.anim.fragment_close_exit);
+        transaction.addToBackStack(null);
+        transaction.replace(R.id.onboarding_fragment, fragment);
+        transaction.commit();
+        fragment_state = state;
     }
 }
