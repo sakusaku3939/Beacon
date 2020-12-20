712d5abe494b1f4ff61db46dedca963507153c0b
sakusaku3939 <yuukiyuuki223@gmail.com>
2020-12-20 20:02:31 +0900

    Preferenceテスト処理追加

--- app/src/main/java/com/sakusaku/beacon/ui/account/AccountFragment.kt
+++ app/src/main/java/com/sakusaku/beacon/ui/account/AccountFragment.kt
@@ -1,5 +1,6 @@
 package com.sakusaku.beacon.ui.account
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,18 +8,26 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.preference.PreferenceFragmentCompat
 import com.sakusaku.beacon.FirebaseAuthUtils
 import com.sakusaku.beacon.R
 
-class AccountFragment : Fragment() {
+class AccountFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preference_app_setting, rootKey)
+    }
+
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val accountViewModel = ViewModelProvider(this).get(AccountViewModel::class.java)
+//        super.onCreateView(inflater, container, savedInstanceState)?.apply {
+//            setBackgroundColor(Color.parseColor("#f9f9f9"))
+//        }
+//        val accountViewModel = ViewModelProvider(this).get(AccountViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_account, container, false)
-        val textView = root.findViewById<TextView?>(R.id.text_account)
-        accountViewModel.getText()?.observe(viewLifecycleOwner, {
-            textView.text = FirebaseAuthUtils.getUserProfile()["name"].toString()
-        })
+//        val textView = root.findViewById<TextView?>(R.id.text_account)
+//        accountViewModel.getText()?.observe(viewLifecycleOwner, {
+//            textView.text = FirebaseAuthUtils.getUserProfile()["name"].toString()
+//        })
         return root
     }
 }
