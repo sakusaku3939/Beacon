a323f04203db076bbdcc72c91a0e57d0f7312998
AokiYuki <yuukiyuuki223@gmail.com>
2020-12-29 05:52:40 +0900

    設定画面を追加

--- app/src/main/java/com/sakusaku/beacon/ui/account/AccountFragment.kt
+++ app/src/main/java/com/sakusaku/beacon/ui/account/AccountFragment.kt
@@ -5,29 +5,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.preference.PreferenceFragmentCompat
-import com.sakusaku.beacon.FirebaseAuthUtils
 import com.sakusaku.beacon.R
 
 class AccountFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preference_app_setting, rootKey)
     }
-
+//
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?, savedInstanceState: Bundle?): View? {
-//        super.onCreateView(inflater, container, savedInstanceState)?.apply {
-//            setBackgroundColor(Color.parseColor("#f9f9f9"))
-//        }
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+        view?.setBackgroundColor(Color.parseColor("#F5F5F5"))
+//        val test = view?.findViewById<PreferenceCategory>(R.)
 //        val accountViewModel = ViewModelProvider(this).get(AccountViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_account, container, false)
+//        val root = inflater.inflate(inflater, container, false)
 //        val textView = root.findViewById<TextView?>(R.id.text_account)
 //        accountViewModel.getText()?.observe(viewLifecycleOwner, {
 //            textView.text = FirebaseAuthUtils.getUserProfile()["name"].toString()
 //        })
-        return root
+        return view
     }
 }
