cedd0e8df554e405c2c9eb992190e25140e7d742
AokiYuki <yuukiyuuki223@gmail.com>
2021-01-02 11:58:41 +0900

    ログアウト後の再起動処理追加

--- app/src/main/java/com/sakusaku/beacon/ui/account/AccountPreference.kt
+++ app/src/main/java/com/sakusaku/beacon/ui/account/AccountPreference.kt
@@ -1,11 +1,13 @@
 package com.sakusaku.beacon.ui.account
 
 import android.content.Context
+import android.content.Intent
+import android.os.Process
 import android.util.AttributeSet
 import androidx.appcompat.app.AlertDialog
 import androidx.preference.Preference
-import androidx.preference.PreferenceManager
 import androidx.preference.PreferenceViewHolder
+import com.sakusaku.beacon.MainActivity
 import com.sakusaku.beacon.R
 
 
@@ -23,7 +25,14 @@ class AccountPreference @JvmOverloads constructor(context: Context?, attrs: Attr
                     .setTitle("ログアウト")
                     .setMessage("この端末からログアウトします。よろしいですか？")
                     .setPositiveButton("はい") { _, _ ->
-                        android.os.Process.killProcess(android.os.Process.myPid())
+                        val restartIntent = Intent(Intent.ACTION_MAIN)
+                        restartIntent.setClassName(context.packageName, MainActivity::class.java.name)
+                        restartIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        context.startActivity(restartIntent)
+                        // TODO: Firebaseからサインアウトする
+                        // TODO: MainActivityでログインの有無を確認する処理の追加
+
+                        Process.killProcess(Process.myPid())
                     }
                     .setNegativeButton("キャンセル") { _, _ -> }
                     .show()
