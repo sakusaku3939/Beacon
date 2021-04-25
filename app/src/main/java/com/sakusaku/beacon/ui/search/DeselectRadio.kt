98f2a142d38993c2eaad68f3cbdde2e517acc19b
AokiYuki <yuukiyuuki223@gmail.com>
2021-04-25 17:40:28 +0900

    ラジオボタン再タップで選択を解除できる処理追加

---
+++ app/src/main/java/com/sakusaku/beacon/ui/search/DeselectRadio.kt
@@ -0,0 +1,23 @@
+package com.sakusaku.beacon.ui.search
+
+import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
+
+class DeselectRadio(private val root: View, private val radioGroup: RadioGroup) {
+    private var isRadioButtonCheck = false
+
+    init {
+        radioGroup.setOnCheckedChangeListener { _, _ -> isRadioButtonCheck = false }
+    }
+
+    fun registerRadioButtonClickListener(idList: List<Int>) {
+        for (id in idList) root.findViewById<RadioButton>(id).setOnClickListener { v ->
+            if (v.id == radioGroup.checkedRadioButtonId && isRadioButtonCheck) {
+                radioGroup.clearCheck()
+            } else {
+                isRadioButtonCheck = true
+            }
+        }
+    }
+}
