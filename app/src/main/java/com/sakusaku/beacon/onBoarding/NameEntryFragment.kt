e2ad9059134de9803ccce5cece8f31555a5eb931
AokiYuki <yuukiyuuki223@gmail.com>
2021-04-29 11:04:19 +0900

    半角文字の時と全角文字が含まれている時で名前の制限文字数を変更

--- app/src/main/java/com/sakusaku/beacon/onBoarding/NameEntryFragment.kt
+++ app/src/main/java/com/sakusaku/beacon/onBoarding/NameEntryFragment.kt
@@ -2,6 +2,7 @@ package com.sakusaku.beacon.onBoarding
 
 import android.os.Bundle
 import android.text.Editable
+import android.text.InputFilter
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
@@ -56,11 +57,19 @@ class NameEntryFragment : Fragment() {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun afterTextChanged(s: Editable) {
+                // 改行を削除
                 for (i in s.length - 1 downTo 0) {
                     if (s[i] == '\n') {
                         s.delete(i, i + 1)
                         return
                     }
+                }
+                // 半角文字のみ入力されているかで制限文字数を変更
+                if (Regex("^[ -~｡-ﾟ]+\$").containsMatchIn(editText.text)) {
+                    editText.filters = arrayOf(InputFilter.LengthFilter(12))
+                } else {
+                    if (editText.text.length > 8) editText.text.delete(8, editText.text.length)
+                    editText.filters = arrayOf(InputFilter.LengthFilter(8))
                 }
             }
         })
