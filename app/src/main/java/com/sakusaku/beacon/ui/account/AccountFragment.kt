7e7854c6d62660b0850ac30a196a2234f058e82b
Aokiti <yuukiyuuki223@gmail.com>
2021-10-31 05:39:16 +0900

    プライバシーポリシーを追加

--- app/src/main/java/com/sakusaku/beacon/ui/account/AccountFragment.kt
+++ app/src/main/java/com/sakusaku/beacon/ui/account/AccountFragment.kt
@@ -1,6 +1,7 @@
 package com.sakusaku.beacon.ui.account
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -17,8 +18,10 @@ import com.sakusaku.beacon.firebase.FirestoreUtils
 
 
 class AccountFragment : PreferenceFragmentCompat() {
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
         val view = super.onCreateView(inflater, container, savedInstanceState)
         view?.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.light_gray))
         return view
@@ -37,7 +40,10 @@ class AccountFragment : PreferenceFragmentCompat() {
                 FirebaseAuthUtils.signOut()
 
                 val restartIntent = Intent(Intent.ACTION_MAIN)
-                restartIntent.setClassName(requireContext().packageName, MainActivity::class.java.name)
+                restartIntent.setClassName(
+                    requireContext().packageName,
+                    MainActivity::class.java.name
+                )
                 restartIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
                 requireContext().startActivity(restartIntent)
 
@@ -61,6 +67,13 @@ class AccountFragment : PreferenceFragmentCompat() {
         val pref = PreferenceManager.getDefaultSharedPreferences(context)
         val disclosureRange = findPreference<ListPreference>("preference_disclosure_range")
         disclosureRange?.isEnabled = !pref.getBoolean("isBeaconScan", false)
+
+        // プライバシーポリシーへのIntentを設定
+        val privacyPolicy = findPreference<PreferenceScreen>("preference_privacy_policy")
+        privacyPolicy?.intent = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("https://github.com/sakusaku3939/Beacon/blob/master/privacy-policy.md")
+        )
 
         // OSSライセンス表示画面へのIntentを設定
         val oss = findPreference<PreferenceScreen>("preference_oss_license")
